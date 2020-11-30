f3d438faa760e07b21d69bd9807508e08d2fdddc
Lucas Pereira <lucasp28.contato@gmail.com>
2020-11-30 19:36:15 -0300

    feat: add util Trait for json responses

---
+++ app/Utils/ApiResponse.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace App\Utils;
+
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\App;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use App\Enums\Responses;
+
+trait ApiResponse
+{
+
+    /**
+     * Return HTTP Continue (100)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function continues()
+    {
+        $args = func_get_args();
+        return $this->response($this->getData($args), Responses::HTTP_CONTINUE);
+    }
+
+    /**
+     * Return HTTP Success (200)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function success()
+    {
+        $args = func_get_args();
+        return $this->response($this->getData($args), Responses::HTTP_OK);
+    }
+
+    /**
+     * Return HTTP Created (201)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function created()
+    {
+        $args = func_get_args();
+        return $this->response($this->getData($args), Responses::HTTP_CREATED);
+    }
+
+    /**
+     * Return HTTP Created (204)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function noContent()
+    {
+        $args = func_get_args();
+        return $this->response($this->getData($args), Responses::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * Return HTTP Bad Request (400)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function bad()
+    {
+        $args = func_get_args();
+        return $this->response($this->getData($args), Responses::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * Return HTTP Unauthorized (401)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function unauthorized()
+    {
+        $args = func_get_args();
+        return $this->response($this->getData($args), Responses::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * Return HTTP Not Found (404)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function notFound()
+    {
+        $args = func_get_args();
+        return $this->response($this->getData($args), Responses::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * Return HTTP Not Found (406)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function notAcceptable()
+    {
+        $args = func_get_args();
+        return $this->response($this->getData($args), Responses::HTTP_NOT_ACCEPTABLE);
+    }
+
+    /**
+     * Return HTTP Conflict (409)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function conflict()
+    {
+        $args = func_get_args();
+        return $this->response($this->getData($args), Responses::HTTP_CONFLICT);
+    }
+
+    /**
+     * Return HTTP Bad Request (422)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function unprocessable()
+    {
+        $args = func_get_args();
+        return $this->response($this->getData($args), Responses::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /**
+     * Return HTTP Internal Error (500)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function internalError()
+    {
+        $args = func_get_args();
+        return $this->response($this->getData($args), Responses::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Return HTTP Code
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function response(array $data, $http_code)
+    {
+        return response()->json($data, $http_code);
+    }
+
+    /**
+     * Return Exception into HTTP Internal Error (500)
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function exception(\Exception $ex)
+    {
+        $log = [
+            'error' => $ex->getMessage(),
+            'file' => $ex->getFile(),
+            'line' => $ex->getLine()
+        ];
+
+        $return = [
+            "erro" => "Ocorreu um erro inesperado. Por favor, tente mais tarde."
+        ];
+
+        if (App::environment('local', 'staging')) {
+            $return['server_error'] = $log;
+        }
+
+        return $this->internalError($return);
+    }
+
+    /**
+     * Return entity data array or array blank
+     *
+     * @return array
+     */
+    private function getData($args)
+    {
+        $data = [];
+
+        /* Sem argumentos, retorna array em branco */
+        if (count($args) < 1) {
+            return $data;
+        }
+
+        /* Enviou um array como parâmetro */
+        if (is_array($args[0])) {
+            return $args[0];
+        }
+
+        /* Enviou um Paginador como parâmetro */
+        if (is_a($args[0], LengthAwarePaginator::class)) {
+            $paginator = ($args[0]);
+            $data = $paginator->toArray();
+        }
+
+        /* Enviou uma Coleção como parâmetro */
+        if (is_a($args[0], Collection::class)) {
+            $collection = ($args[0]);
+            $data = $collection->toArray();
+        }
+
+        /* Enviou uma Entidade como parâmetro */
+        if (is_a($args[0], Model::class)) {
+            $entity = ($args[0]);
+            // $entity->autoload();
+            // $data['data'] = $entity->toArray();
+            $data = $entity->toArray();
+        }
+
+        return $data;
+    }
+}
