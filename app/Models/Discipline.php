107d2e4d0c622348ae5c040035bba6d023f45f19
Lucas Pereira <lucasp28.contato@gmail.com>
2020-12-01 06:42:32 -0300

    feat: ensure Discipline can be added with correct relationships

---
+++ app/Models/Discipline.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Discipline extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'difficulty_id',
+        'teacher_id'
+    ];
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class, 'teacher_id', 'id');
+    }
+
+    public function difficulty()
+    {
+        return $this->belongsTo(Difficulty::class, 'difficulty_id', 'id');
+    }
+}
---
+++ database/migrations/2020_12_01_091720_create_disciplines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDisciplinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('disciplines', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('difficulty_id')->references('id')->on('difficulties');
+            $table->foreignId('teacher_id')->references('id')->on('teachers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('disciplines');
+    }
+}
