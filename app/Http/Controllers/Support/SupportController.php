3119f850e2c0172a799dc832a1d4fd6865f032ed
Lucas Pereira <lucasp28.contato@gmail.com>
2020-12-03 00:54:12 -0300

    feat: ensure Student add feedback validate attribute

--- app/Http/Controllers/Support/SupportController.php
+++ app/Http/Controllers/Support/SupportController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers\Support;
 
 use App\Http\Controllers\Controller;
-use App\Utils\ApiResponse;
-use Illuminate\Http\Request;
+use App\Http\Requests\CreateSupportFeedbackRequest;
+use App\Utils\ApiResponse;;
 use Illuminate\Support\Facades\Auth;
 
 class SupportController extends Controller
 {
     use ApiResponse;
 
-    public function create(Request $request)
+    public function create(CreateSupportFeedbackRequest $request)
     {
         $message = $request->post('message');
         Auth::user()->addFeedback($message);
