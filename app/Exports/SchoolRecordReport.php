79d6853eecb508426437f7883e76622af92aca13
Lucas Pereira <lucasp28.contato@gmail.com>
2020-12-04 03:22:08 -0300

    refactor: change pdf file informations

--- app/Exports/SchoolRecordReport.php
+++ app/Exports/SchoolRecordReport.php
@@ -12,7 +12,7 @@ class SchoolRecordReport
 {
     public function handle(Student $student, Collection $schoolRecords)
     {
-        $pdf = PDF::loadView('pdf.school-record', ['schoolRecords' => $schoolRecords]);
+        $pdf = PDF::loadView('pdf.school-record', ['student' => $student, 'schoolRecords' => $schoolRecords]);
         $fileName = "school-records/Historico_$student->registration.pdf";
         $pdf->save(storage_path() . "/$fileName");
     }
--- resources/views/pdf/school-record.blade.php
+++ resources/views/pdf/school-record.blade.php
@@ -1,7 +1,29 @@
 <html>
+    <body>
+        <h1>Histórico</h1>
+        <strong>Nome:</strong> {{$student->name}} <br>
+        <strong>Matrícula:</strong> {{$student->registration}} <br>
+        <strong>Curso:</strong> {{$student->course->name}} <br>
+        <strong>Período:</strong> {{$student->current_semester}} <br>
 
-@forelse($schoolRecords as $record)
-    Nome da Disciplina: {{$record['discipline_name']}}
-@empty
-@endforelse
+        <hr>
+        <table>
+            <tr>
+                <th>Nome da Disciplina</th>
+                <th>Professor</th>
+                <th>Status</th>
+                <th>Nota final</th>
+            </tr>
+                @forelse($schoolRecords as $record)
+                    <tr>
+                        <td>{{$record['discipline_name']}}</td>
+                        <td>{{$record['discipline_teacher']}}</td>
+                        <td>{{$record['status']}}</td>
+                        <td>{{$record['final_grade'] ?? '-'}}</td>
+                    </tr>
+                @empty
+                    Nenhum dado encontrado.
+                @endforelse
+        </table>
+    </body>
 </html>
